e54ab37b86a7e98ef036d3d5119d9a1d2a6df4a0
d3uceY <onyekwelujesse1234@gmail.com>
2026-05-03 23:42:41 +0100

    refactor(main): restructure app options initialization and maintain window state logic

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"runtime"
 )
 
 //go:embed all:frontend/dist
@@ -17,14 +18,12 @@ func main() {
 	// Create an instance of the app structure
 	app := NewApp()
 
-	// Create application with options
-	err := wails.Run(&options.App{
-		Title:      "Avnac",
-		Width:      600,
-		Height:     600,
-		MinWidth:   600,
-		MinHeight:  600,
-		WindowStartState: options.Maximised,
+	appOptions := &options.App{
+		Title:            "Avnac",
+		Width:            600,
+		Height:           600,
+		MinWidth:         600,
+		MinHeight:        600,
 		AssetServer: &assetserver.Options{
 			Assets:     assets,
 			Middleware: app.MediaProxyMiddleware(),
@@ -40,7 +39,14 @@ func main() {
 			app.Config,
 			app.Secrets,
 		},
-	})
+	}
+
+	if runtime.GOOS != "darwin" {
+		appOptions.WindowStartState = options.Maximised
+	}
+
+	// Create application with options
+	err := wails.Run(appOptions)
 
 	if err != nil {
 		println("Error:", err.Error())
